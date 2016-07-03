c6eef35182aa4ee1ffddf12e10ccba69436ba2d3
jxnblk <jacksonblack@gmail.com>
2016-07-03 10:12:54 -0400

    Move jss version files

--- src/index.js
+++ src/index.js
@@ -1,70 +1,20 @@
 
-import hash from 'murmurhash-js/murmurhash3_gc'
-import createRules from './create-rules'
+import sheet from './jss-sheet'
 
-export let styleTag = null
-export let cache = {}
-
-const cxs = (style) => {
+const cxs = (...args) => {
   const classNames = []
-  const hashname = 'cxs-' + hash(JSON.stringify(style), 128)
-  const rules = createRules(hashname, style)
 
-  rules.forEach(rule => {
-    if (!/\:/.test(rule.selector)) {
-      classNames.push(rule.selector.replace(/^\./, ''))
-    }
-    if (!cache[rule.id]) {
-      cache[rule.id] = rule
+  args.forEach(arg => {
+    if (typeof arg === 'string') {
+      classNames.push(arg)
+    } else {
+      const rule = sheet.addRule('cxs', arg)
+      classNames.push(rule.className)
     }
   })
 
-  cxs.attach()
   return classNames.join(' ')
 }
-
-cxs.attach = () => {
-  if (typeof document === 'undefined') {
-    console.warn('Cannot attach stylesheet without a document')
-    return
-  }
-
-  const rules = cxs.rules
-  styleTag = styleTag || document.getElementById('cxs')
-
-  if (styleTag === null) {
-    styleTag = document.createElement('style')
-    styleTag.id = 'cxs'
-    document.head.appendChild(styleTag)
-    cxs.sheet = styleTag.sheet
-  }
-
-  for (var i = 0; i < rules.length; i++) {
-    const rule = rules[i]
-    try {
-      cxs.sheet.insertRule(rule.css, cxs.sheet.cssRules.length)
-    } catch (e) {}
-  }
-}
-
-cxs.clearCache = () => cache = {}
-
-Object.defineProperty(cxs, 'rules', {
-  get () {
-    return Object.keys(cache || {})
-      .map(k => cache[k] || false)
-      .filter(r => r.css.length)
-      .sort((a, b) => a.order - b.order)
-  }
-})
-
-Object.defineProperty(cxs, 'css', {
-  get () {
-    return cxs.rules
-      .map(r => r.css)
-      .join('')
-  }
-})
 
 export default cxs
 
