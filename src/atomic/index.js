f366636e6c3cb4d06266d9461de1fb11ca8b604d
Brent Jackson <jxnblk@gmail.com>
2016-12-20 20:53:11 -0500

    Perf improvements for atomic mode

--- src/atomic/index.js
+++ src/atomic/index.js
@@ -1,24 +1,101 @@
 
-import addPx from 'add-px-to-style'
-import { insert, reset, css } from '../sheet'
 import hash from '../hash'
 import {
-  createStylesArray,
+  addPx,
   clean,
   combine,
   hyphenate
 } from '../util'
 import shorthands from './shorthands'
+import { StyleSheet } from 'glamor/lib/sheet'
+
+export const sheet = new StyleSheet()
+
+sheet.inject()
+
+export const cache = {}
+
+export const reset = () => {
+  for (let key in cache) {
+    delete cache[key]
+  }
+  sheet.flush()
+}
+
+export const css = () => sheet.rules()
+  .map(r => r.cssText)
+  .join('')
 
 const cxs = (style) => {
-  const rules = createStylesArray(style)
-    .map(createRule)
-  const classNames = rules
-    .map(rule => rule.className)
-
-  rules.forEach(insert)
-
+  const classNames = parse(style)
   return classNames.join(' ')
+}
+
+const parse = (obj, media, children = '') => {
+  const classNames = []
+
+  for (let key in obj) {
+    const value = obj[key]
+    const type = typeof value
+
+    if (type === 'string' || type === 'number') {
+      classNames.push(createStyle(key, value, media, children))
+      continue
+    }
+
+    if (Array.isArray(value)) {
+      value.forEach(val => {
+        classNames.push(
+          createStyle(key, value, media, children)
+        )
+      })
+      continue
+    }
+
+    if (/^:/.test(key)) {
+      parse(value, media, children + key)
+        .forEach(className => {
+          classNames.push(className)
+        })
+      continue
+    }
+
+    if (/^@media/.test(key)) {
+      parse(value, key, children)
+        .forEach(className => {
+          classNames.push(className)
+        })
+      continue
+    }
+
+    parse(value, media, children + ' ' + key)
+      .forEach(className => {
+        classNames.push(className)
+      })
+    continue
+  }
+
+  return classNames
+}
+
+const createStyle = (key, value, media, children = '') => {
+  const prefix = (media || '') + children
+  const id = key + value + prefix
+  const dupe = cache[id]
+
+  if (dupe) return dupe
+
+  const className = createClassName(key, value, prefix)
+  const selector = '.' + className + children
+  const prop = hyphenate(key)
+  const val = addPx(key, value)
+  const rule = `${selector}{${prop}:${val}}`
+  const css = media ? `${media}{${rule}}` : rule
+
+  sheet.insert(css)
+  cache[id] = className
+
+  return className
 }
 
 const abbr = (str) => str
@@ -40,24 +117,8 @@ const createClassName = (prop, value, prefix) => {
   return className
 }
 
-const createRule = ({ id, key, value, parent = '', selector = '' }) => {
-  const prop = hyphenate(key)
-  const prefix = combine()(parent, selector)
-  const className = createClassName(prop, value, prefix)
-  const sel = combine()('.', className, selector)
-  const rule = `${sel}{${prop}:${addPx(key, value)}}`
-  const css = parent ? `${parent}{${rule}}` : rule
-
-  return {
-    id: className + id,
-    className,
-    css
-  }
-}
-
 cxs.reset = reset
 cxs.css = css
 
-export { sheet, cache, reset, css } from '../sheet'
 export default cxs
 
