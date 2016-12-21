ccdbfcf78146bcc915f5e4e118a4029d734810f1
Brent Jackson <jxnblk@gmail.com>
2016-12-20 20:53:40 -0500

    Perf improvements for monolithic mode

--- src/monolithic/index.js
+++ src/monolithic/index.js
@@ -1,66 +1,99 @@
 
-import assign from 'object-assign'
-import addPx from 'add-px-to-style'
+import { StyleSheet } from 'glamor/lib/sheet'
 import hash from '../hash'
-import { insert, css, reset } from '../sheet'
-import {
-  createStylesArray,
-  hyphenate,
-  objToArr,
-  getStringArgs,
-  getObjectArgs
-} from '../util'
+import { addPx, hyphenate } from '../util'
+
+export const sheet = new StyleSheet()
+
+sheet.inject()
+
+export let cache = {}
+
+export const reset = () => {
+  for (let key in cache) {
+    delete cache[key]
+  }
+  sheet.flush()
+}
+
+export const css = () => sheet.rules()
+  .map(rule => rule.cssText)
+  .join('')
 
 const cxs = (...args) => {
-  const selectors = args.reduce(getStringArgs, [])
-  const style = args.reduce(getObjectArgs, {})
+  let selector
+  if (typeof args[0] === 'string') {
+    selector = args[0]
+  }
+  const style = selector ? args[1] : args[0]
   const hashname = hash(JSON.stringify(style))
-  const selector = selectors.length ? selectors.join(', ') : '.' + hashname
+  selector = selector || '.' + hashname
 
-  const styles = createStylesArray(style)
-    .reduce(group, {})
-
-  const rules = createRules(selector, styles)
-
-  rules.forEach(insert)
+  parse(selector, style)
 
   return hashname
 }
 
-const group = (a = {}, b) => {
-  const { id } = b
-  a[id] = a[id] || assign({}, b, { declarations: [] })
-  a[id].declarations.push(b)
-  return a
+const parse = (selector, obj, media, children = '') => {
+  const rules = []
+
+  for (let key in obj) {
+    const value = obj[key]
+    const type = typeof value
+
+    if (type === 'number' || type === 'string') {
+      rules.push(
+        createRule(selector + children, key, value, media)
+      )
+      continue
+    }
+
+    if (/^:/.test(key)) {
+      parse(selector, value, media, children + key)
+        .forEach(rule => {
+          rules.push(rule)
+        })
+      continue
+    }
+
+    if (/^@media/.test(key)) {
+      parse(selector, value, key, children)
+        .forEach(rule => {
+          rules.push(rule)
+        })
+      continue
+    }
+
+    parse(selector, value, media, children + ' ' + key)
+      .forEach(rule => {
+        rules.push(rule)
+      })
+    continue
+  }
+
+  return rules
 }
 
-const createRules = (rootSelector, styles) => objToArr(styles)
-  .map(({ key, value }) => {
-    const { selector, declarations, parent } = value
-    const rule = createRule(rootSelector + selector)(declarations)
-    const css = parent ? `${parent}{${rule}}` : rule
+const createRule = (selector, key, value, media) => {
+  const prop = hyphenate(key)
+  const val = addPx(key, value)
+  const rule = `${selector}{${prop}:${val}}`
+  const css = media ? `${media}{${rule}}` : rule
+  const id = hash(css)
 
-    return {
-      id: rootSelector + '-' + key,
-      css
-    }
-  })
+  if (cache[id]) return
 
-const createRule = (selector) => (declarations) => {
-  const body = declarations.map(({ key, value }) => (
-    hyphenate(key) + ':' + addPx(key, value)
-  )).join(';')
-  return `${selector}{${body}}`
+  sheet.insert(css)
+  cache[id] = true
+
+  return {
+    id,
+    css
+  }
 }
 
 cxs.reset = reset
 cxs.css = css
 
-export {
-  sheet,
-  cache,
-  reset,
-  css
-} from '../sheet'
 export default cxs
 
