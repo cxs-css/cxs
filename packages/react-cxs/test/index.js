b35b3d25d3a5f2113f4cc346891c76a256a9b53a
Brent Jackson <jxnblk@gmail.com>
2016-12-27 11:08:43 -0500

    Update cxs package

--- packages/react-cxs/test/index.js
+++ packages/react-cxs/test/index.js
@@ -1,8 +1,8 @@
 
 import test from 'ava'
-import React from 'react'
 import TestUtils from 'react-addons-test-utils'
 import jsdom from 'jsdom-global'
+import { StyleSheet } from 'glamor/lib/sheet'
 import reactCxs, { cxs } from '../src'
 
 jsdom('<html></html>')
@@ -14,12 +14,12 @@ const Root = (props) => (
 
 test('renders', t => {
   t.notThrows(() => {
-    r.render(<Root />)
+    r.render(reactCxs(Root))
   })
 })
 
 test('renders a div', t => {
-  r.render(<Root />)
+  r.render(reactCxs(Root))
   const tree = r.getRenderOutput()
   t.is(tree.type, 'div')
 })
@@ -28,32 +28,18 @@ test('accepts object classNames', t => {
   const cx = {
     color: 'tomato'
   }
-  r.render(<Root className={cx} />)
+  r.render(reactCxs(Root, { className: cx }))
   const tree = r.getRenderOutput()
-  t.regex(tree.props.className, /^cxs/)
+  t.regex(tree.props.className, /^c\-tomato/)
 })
 
 test('accepts string classNames', t => {
-  r.render(<Root className='hello' />)
+  r.render(reactCxs(Root, { className: 'hello' }))
   const tree = r.getRenderOutput()
   t.is(tree.props.className, 'hello')
 })
 
-test('attaches stylesheet to document', async t => {
-  t.plan(3)
-  const getTag = () => new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const style = document.querySelector('style')
-      resolve(style)
-    }, 20)
-  })
-  const style = await getTag()
-  t.is(style.tagName, 'STYLE')
-  t.true(style.sheet.cssRules.length > 0)
-  t.is(style.sheet.cssRules[0].style.color, 'tomato')
-})
-
 test('add rules to cxs', t => {
-  t.regex(cxs.css, /tomato/)
+  t.regex(cxs.css(), /tomato/)
 })
 
